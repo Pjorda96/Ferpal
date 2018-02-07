209263dd65a244fd5e93414f6b693d467bb416c7
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-02-07 10:03:40 +0100

    fj

--- Actividad_cesar/codCesar.php
+++ Actividad_cesar/codCesar.php
@@ -1,3 +1,15 @@
 <?php
+$contadorGlobal=0;
+//para un numero par de la palabra
+function par(){
+  arrayResultado[$contadorGlobal]=arrayPar[$contadorGlobal];
+  
+  $contadorGlobal=$contadorGlobal+1;
+}
+function impar(){
+  arrayResultado[$contadorGlobal]=arrayPar[$contadorGlobal+28];
+
+  $contadorGlobal=$contadorGlobal+1;
+}
 
 ?>
