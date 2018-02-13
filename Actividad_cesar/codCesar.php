24de3d33209dcc3d42898dd559b2f4584d461856
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-02-13 08:51:37 +0100

    mods

--- Actividad_cesar/codCesar.php
+++ Actividad_cesar/codCesar.php
@@ -1,30 +1,37 @@
 <?php
 $fraseGET=$_POST['frase'];
-//añadir frase al array
+
+//añadir frase al array frase
 $frase[];
+for ($i=0; $i<=(strlen($fraseGET)-1);$i++){
+  $fraseGET[$i]=$frase{$i};
+}
 
-while ()
-
-$contadorGlobal=;
-$contadorFila=$contadorGlobal/7;
-
+$contadorGlobal=0;
+//$contadorFila=$contadorGlobal/7;
 
 //para un numero par de la palabra
 function par(){
 
-
-  arrayResultado[$contadorGlobal]=arrayPar[$contadorGlobal][$contadorFila];
+  arrayResultado[$contadorGlobal][$contadorFila]=arrayPar[$contadorGlobal][$contadorFila];
 
   $contadorGlobal++;
 }
 function impar(){
-  arrayResultado[$contadorGlobal]=arrayPar[$contadorGlobal][$contadorFila];
+  arrayResultado[$contadorGlobal][$contadorFila]=arrayPar[$contadorGlobal][$contadorFila];
 
   $contadorGlobal++;
 }
 
+//se pasa el carácter que toque según si la posición en la frase es par o impar
+while($contadorGlobal<strlen($fraseGET)){
+  if ($contadorGlobal%2==0) par($frase{$contadorGlobal});
+  else impar($frase{$contadorGlobal});
+}
 
-  $descodificada=$_POST['Frase_descodificada'];
-  echo $descodificada ;
+
+$descodificada=$_POST['Frase_descodificada'];
+echo $descodificada ; //wtf es esto??
+
+header('Location: formulario_cesar.php');
 ?>
-<a href="formulario_cesar.php">Volver a la página principal</a>
