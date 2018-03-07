1ebd46a0ee51715f6da0b25ba7e988ccda2d58c8
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-03-07 10:53:52 +0100

    decoder pablo

--- descodificado/decodificador_pablo.php
+++ descodificado/decodificador_pablo.php
@@ -2,18 +2,19 @@
 define("MIN",32);
 define("MAX", 122);
 $fmove = 16;
-$input_string = "Una cosa es ser solidario, y otra es serlo a cambio de nada. Mariano Rajoy";
-//$input_string = $_POST['caja'];
+//$input_string = "Una cosa es ser solidario, y otra es serlo a cambio de nada. Mariano Rajoy";
+$input_string = "ex#CK_w\"BLd)AKb'?Sdr &=K_0Pn3G`w\"BLd)AKbs,LRrz%7>L`%.8Xkqz%Ssek'5;Nb'y$3Ge";
+echo $input_string;
 $input_length = strlen($input_string);
 $input_array = [];
 for ($i=0; $input_length > $i ; $i++) {
-  $char = $input_string[$i];
+  $char = $input_string[$input_length-$i];
   $char_val = ord($char);
   if ($char_val + $fmove > MAX) {
-    $a = $char_val + $fmove - MAX;
+    $a = $char_val - $fmove + MAX;
     $result = MIN - 1 + $a;
   } else {
-    $result = $char_val + $fmove;
+    $result = $char_val - $fmove;
   }
   if ($result > MAX) {
     $a = $result - MAX;
@@ -22,10 +23,9 @@ for ($i=0; $input_length > $i ; $i++) {
   $input_array[] = $result;
   $fmove = $result;
 }
-// Muestra el código numérico ascii de cada caracter
+
 $numbers = implode(",", $input_array);
-//echo $numbers . "<br><br>";
-// Esto muestra los caracteres que entregaremos al resto de compañeros
+
 for ($i=0; $i < $input_length ; $i++) {
   $ascii = $input_array[$i];
   echo "<b>" . chr($ascii) . "</b>";
